1bd6d6a9555ea7255719d3efb6536698c9c5dcc6
fari06200 <92125440+fari06200@users.noreply.github.com>
2023-03-30 19:52:41 +0200

    init

---
+++ README.md
@@ -0,0 +1,23 @@
+# Formulaire de connexion animé
+
+## Formulaire de connexion animé
+
+- Formulaire de connexion animé utilisant HTML CSS et JavaScript
+- Il contient un arrière-plan élégant et magnifique.
+- Avec animation d'étiquettes flottantes.
+- Et un bouton pour afficher et masquer le mot de passe.
+- Développé d'abord avec la méthodologie Mobile First, puis pour le bureau.
+- Compatible avec tous les appareils mobiles.
+
+# Animated Login Form
+
+## Animated Login Form
+
+- Animated Login Form Using HTML CSS & JavaScript
+- It contains a stylish and beautiful background.
+- With animation of floating labels.
+- And a button to show and hide the password.
+- Developed first with the Mobile First methodology, then for desktop.
+- Compatible with all mobile devices.
+
+![preview img](/preview.png)
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,31 @@
-/*=============== SHOW HIDDEN - PASSWORD ===============*/
+/*========== AFFICHER CACHÉ - MOT DE PASSE ==========*/
+/*========== SHOW HIDDEN - PASSWORD ==========*/
+const showHiddenPass = (loginPass, loginEye) => {
+  const input = document.getElementById(loginPass),
+    iconEye = document.getElementById(loginEye);
+
+  iconEye.addEventListener("click", () => {
+    // Changer le mot de passe en texte
+    // Change password to text
+    if (input.type === "password") {
+      // Passer au texte
+      // Switch to text
+      input.type = "text";
+      // Changement d'icône
+      // Icon change
+      iconEye.classList.add("ri-eye-line");
+      iconEye.classList.remove("ri-eye-off-line");
+    } else {
+      // Changement de mot de passe
+      // Change to password
+      input.type = "password";
+
+      // Icon change
+      // Changement d'icône
+      iconEye.classList.remove("ri-eye-line");
+      iconEye.classList.add("ri-eye-off-line");
+    }
+  });
+};
+
+showHiddenPass("login-pass", "login-eye");
